0dcedbea4e97ecdc4ae06abc3e208cb0bbcaa93d
Muhammad Sidratul Muntaha <muhammad.mochi@gmail.com>
2013-11-07 22:14:05 +0700

    tambah kelas umkm dan kategoriUMKM

---
+++ web/solusi-umkm/src/main/java/com/sidratul/solusiumkm/model/KategoriUMKM.java
@@ -0,0 +1,23 @@
+package com.sidratul.solusiumkm.model;
+
+public class KategoriUMKM {
+    private Integer idKategoriUMKM;
+    private String kategoriUMKM;
+
+    public Integer getIdKategoriUMKM() {
+        return idKategoriUMKM;
+    }
+
+    public void setIdKategoriUMKM(Integer idKategoriUMKM) {
+        this.idKategoriUMKM = idKategoriUMKM;
+    }
+
+    public String getKategoriUMKM() {
+        return kategoriUMKM;
+    }
+
+    public void setKategoriUMKM(String kategoriUMKM) {
+        this.kategoriUMKM = kategoriUMKM;
+    }
+            
+}
---
+++ web/solusi-umkm/src/main/java/com/sidratul/solusiumkm/model/UMKM.java
@@ -0,0 +1,96 @@
+package com.sidratul.solusiumkm.model;
+
+public class UMKM {
+    private Integer idUMKM;
+    private String namaUMKM;
+    private String pemilikUMKM;
+    private String detailProduk;
+    private KategoriUMKM ku;
+    private String visi;
+    private String misi;
+    private String alamat;
+    private String noTelp;
+    private String email;
+
+    public Integer getIdUMKM() {
+        return idUMKM;
+    }
+
+    public void setIdUMKM(Integer idUMKM) {
+        this.idUMKM = idUMKM;
+    }
+
+    public String getNamaUMKM() {
+        return namaUMKM;
+    }
+
+    public void setNamaUMKM(String namaUMKM) {
+        this.namaUMKM = namaUMKM;
+    }
+
+    public String getPemilikUMKM() {
+        return pemilikUMKM;
+    }
+
+    public void setPemilikUMKM(String pemilikUMKM) {
+        this.pemilikUMKM = pemilikUMKM;
+    }
+
+    public String getDetailProduk() {
+        return detailProduk;
+    }
+
+    public void setDetailProduk(String detailProduk) {
+        this.detailProduk = detailProduk;
+    }
+
+    public KategoriUMKM getKu() {
+        return ku;
+    }
+
+    public void setKu(KategoriUMKM ku) {
+        this.ku = ku;
+    }
+
+    public String getVisi() {
+        return visi;
+    }
+
+    public void setVisi(String visi) {
+        this.visi = visi;
+    }
+
+    public String getMisi() {
+        return misi;
+    }
+
+    public void setMisi(String misi) {
+        this.misi = misi;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
